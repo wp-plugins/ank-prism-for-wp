f89b380b8995e5a19fcda0e953f20658b463ce58
ank91 <ank91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-05 11:57:56 +0000

    tag 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/ank-prism-for-wp/tags/1.6.1@1192749 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+
+/*  Uninstall file for "Ank Prism For WP" Plugin
+*   This file will be used to remove all traces of this plugin when uninstalled
+*/
+
+//if uninstall not called from WordPress do exit
+
+if (!defined('WP_UNINSTALL_PLUGIN'))
+    exit;
+
+/*
+ * remove the database entry created by this plugin
+ */
+
+delete_option('ank_prism_for_wp');
+
